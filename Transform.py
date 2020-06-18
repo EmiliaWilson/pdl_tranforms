84e1b819cadabc98af43510e85a142e719d12bcb
Jake-R-W <31546309+Jake-R-W@users.noreply.github.com>
2020-06-18 16:49:31 -0400

    finished scale, rot, and inverse.
    Need to make compose next

--- Transform.py
+++ Transform.py
@@ -4,13 +4,15 @@ import matplotlib.pyplot as plt
 import astropy.units as units
 from abc import ABC, abstractmethod
 from scipy.spatial.transform import Rotation as R
+import copy
+
 
 class Transforms(ABC):
 
     def __init__(self, name: str, input_coord: np.array, input_unit: units,
                  output_coord: np.array, output_unit: units, parameters: dict,
-                 inverted_flag: bool, reverse_flag: bool, input_dim: int=None,
-                 output_dim: int=None):
+                 non_invertible: bool, reverse_flag: bool, input_dim: int = None,
+                 output_dim: int = None):
         """
         :type name: str
         :type input_coord: np.array
@@ -18,7 +20,7 @@ class Transforms(ABC):
         :type output_coord: np.array
         :type output_unit: astropy.units
         :type parameters: dict
-        :type inverted_flag: bool
+        :type non_invertible: bool
         :type reverse_flag: bool
         :type input_dim: int
         :type output_dim: int
@@ -29,12 +31,14 @@ class Transforms(ABC):
         self.output_coord = output_coord
         self.output_unit = output_unit
         self.parameters = parameters
-        self.inverted_flag = inverted_flag
+        self.non_invertible = non_invertible
         self.reverse_flag = reverse_flag
         self.input_dim = input_dim
         self.output_dim = output_dim
 
     def composition(self):
+        # f(g(h(x))) == composition([h, g, f]) or composition(f, g, h)
+        # look at function composition in python to find standard but it seems like second is more common.
         pass
 
     def inverse(self):
@@ -71,10 +75,10 @@ class t_identity(Transforms):
     """
 
     def __init__(self, input_coord=None, input_unit=None, output_coord=None,
-                         output_unit=None, parameters=None, inverted_flag=None,
-                         reverse_flag=None, input_dim=0, output_dim=0):
+                 output_unit=None, parameters=None, non_invertible=None,
+                 reverse_flag=None, input_dim=0, output_dim=0):
         super().__init__("identity", input_coord=input_coord, input_unit=input_unit, output_coord=output_coord,
-                         output_unit=output_unit, parameters=parameters, inverted_flag=inverted_flag,
+                         output_unit=output_unit, parameters=parameters, non_invertible=non_invertible,
                          reverse_flag=reverse_flag, input_dim=input_dim, output_dim=output_dim)
 
     def apply(self, data, backward=0):
@@ -108,15 +112,13 @@ class t_linear(Transforms):
 
     def __init__(self, input_coord: np.array, input_unit: units,
                  output_coord: np.array, output_unit: units, parameters: dict,
-                 inverted_flag: bool, reverse_flag: bool, input_dim: int,
+                 non_invertible: bool, reverse_flag: bool, input_dim: int,
                  output_dim: int):
         # this basic implementation doesn't deal with all the cases you see in PDL. They will be implemented later
         # params = {"matrix": None, "scale": None, "rot": 0, "pre": None, "post": None, "dims": None}
 
-
-
         super().__init__("t_linear", input_coord, input_unit, output_coord,
-                         output_unit, parameters, inverted_flag,
+                         output_unit, parameters, non_invertible,
                          reverse_flag, input_dim, output_dim)
 
         # Figuring out the number of dimensions to transform, and, if necessary, generate a new matrix
@@ -141,29 +143,70 @@ class t_linear(Transforms):
                 self.input_dim = self.output_dim = self.parameters['dims']
             else:
                 print("Assuming 2-D transform(set dims options)")
-                self.input_dim = self.output_dim = 2;
+                self.input_dim = self.output_dim = 2
 
             self.parameters['matrix'] = np.zeros([self.input_dim, self.output_dim])
             np.fill_diagonal(self.parameters['matrix'], 1)
 
-
         # Handle rotation option
         rot = self.parameters['rot']
         if rot is not None:
+            if rot is np.ndarray:
+                if np.ndim(rot) == 2:
+                    # rotation matrix, need to use compose
+                    print("composing new matrix")
+                elif np.size(rot) == 3:
+                    rotation = R.from_euler('xyz', [rot[0], rot[1], rot[2]], degrees=True)
+                    rot_matrix = rotation.as_dcm()
+                    # self.parameters['matrix] = compose(rot_matrix, self.parameters['matrix])
+                    # this also works self.parameters['matrix] = np.matmul(rot_matrix, self.parameters['matrix])
+                else:
+                    raise ValueError("Transform.linear got a strange rot option -- giving up.")
 
+            elif rot != 0 and self.parameters['matrix'].shape[0] > 1:
+                theta = np.deg2rad(rot)
+                c, s = np.cos(theta), np.sin(theta)
+                if c < 1e-10:
+                    c = 0
+                if s < 1e-10:
+                    s = 0
+                rot_matrix = np.array(((c, -s), (s, c)))
+                # self.parameters['matrix] = compose(rot_matrix, self.parameters['matrix])
+                # this also works self.parameters['matrix] = np.matmul(rot_matrix, self.parameters['matrix])
 
-
-        # applying scaling
+        # applying scaling. No matrix. Documentation
         if self.parameters['scale'] is not None and type((self.parameters['scale']) is not np.ndarray):
 
             for j in range(self.parameters['matrix'].shape[0]):
                 self.parameters['matrix'][j][j] *= self.parameters['scale']
 
         elif type(self.parameters['scale']) is np.ndarray:
-            if np.ndim(self.parameters['scale']) > 1:
+            if self.parameters['scale'].ndims > 1:
+                raise ValueError("Scale only accepts scalars and 1D arrays")
+            else:
+                # this might be wrong
+                for j in range(self.parameters['matrix'].shape[0]):
+                    self.parameters['matrix'][j][j] *= self.parameters['scale'][j]
 
-
-
+        # need to check for inverse and set inverted flag. Throw error in apply
+        try:
+            self.inv = np.linalg.inv(self.parameters['matrix'])
+        except np.linalg.LinAlgError:
+            self.inv = None
+            self.non_invertible = 1
 
     def apply(self, data, backwards=0):
-        pass
+        if (not backwards and not self.reverse_flag) or (backwards and self.reverse_flag):
+            print("forward")
+            d = self.parameters['matrix'].shape[0]
+            if d > np.shape(data)[0]:
+                raise ValueError(f"Linear transform: transform is {np.shape(data)[0]} data only ")
+
+            x = copy.deepcopy(data[0:d]) + self.parameters['pre']
+            out = copy.deepcopy(data)
+            # out[0:d] = compose
+
+        elif self.non_invertible:
+            print("is going to run inverse")
+        else:
+            print("trying to invert a non-invertible matrix.")
